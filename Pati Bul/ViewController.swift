58515b1b7993c97972b042dfb151c78a9dee2dc4
Görkem Karagöz <gorkem1928karagoz@gmail.com>
2024-12-09 18:36:04 +0300

    Initial Commit

---
+++ Pati Bul/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pati Bul
+//
+//  Created by Görkem Karagöz on 9.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
